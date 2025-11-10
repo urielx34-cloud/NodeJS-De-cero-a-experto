bb6352542c55c31a786fa088a4d870f830887bce
urielrodriguez <urielx34@gmail.com>
2025-11-09 18:44:24 -0800

    config  inicial para hacer tests

--- src/app.ts
+++ src/app.ts
@@ -13,10 +13,10 @@ import { buildLogger } from "./plugins/logger.plugin";
 // const getPokemonById = require('./js-foundation/06-promises');
 
  
-const logger = buildLogger('app.js');
+// const logger = buildLogger('app.js');
 
-logger.log('Hola mundo');
-logger.error('Esto es algo malo');
+// logger.log('Hola mundo');
+// logger.error('Esto es algo malo');
 
 // getPokemonById(4)
 //   .then( ( pokemon ) => console.log({ pokemon }) )
@@ -32,7 +32,7 @@ logger.error('Esto es algo malo');
 // Publicas
 
 
-// ! Referencia a la función factory y uso
+//  !Referencia a la función factory y uso
 // const makePerson = buildMakePerson({ getUUID, getAge });
 
 // const obj = { name: 'John', birthdate: '1985-10-21' };
