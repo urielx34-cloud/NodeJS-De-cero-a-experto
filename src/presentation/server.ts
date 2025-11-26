5b49c256de6060e9ea09e56ee9f9abc0f9f743fe
urielrodriguez <urielx34@gmail.com>
2025-11-25 20:24:09 -0800

    uso de cron para tareas programadas

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,6 +1,13 @@
+import { CronService } from "./cron/cron-service";
 
 export class Server {
     static start() {
-        console.log("Server started");
+        console.log("Server started...");
+        
+        /// patron adapatador ya que sustituye la configuracion nativa de cron por una clase propia
+        CronService.createJob('*/5 * * * * *', () => {
+            const date = new Date();
+            console.log('Cron  se ejecuta cada 5 segundos', date);
+        });
     }
 }
