a257974ac968d78fa7f3510abbd443ba84c0b53e
urielrodriguez <urielx34@gmail.com>
2025-12-03 21:52:10 -0800

    configuracion para conectar a mongo

---
+++ src/data/mongo/init.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+
+interface ConnectionOptions{
+    mongoUrl: string;
+    dbName: string;
+
+}
+
+
+export class MongoDataBase{
+    static async connect(options: ConnectionOptions){
+        const {mongoUrl,dbName} = options;
+
+        try {
+            await mongoose.connect(mongoUrl,{
+                dbName: dbName
+            });
+
+            console.log("mongo Connect")
+        } catch (error) {
+            console.log("Mongo connection error");
+            throw error;
+        }
+    }
+}
