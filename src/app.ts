cd2e96284f4fe6794b95592ea4c72c9f1be78303
urielrodriguez <urielx34@gmail.com>
2025-12-03 21:41:54 -0800

    configuracion para conectar a mongo

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { envs } from './config/plugins/envs.plugin';
+import { MongoDataBase } from './data/mongo/';
 import { Server } from './presentation/server';
 import  'dotenv/config';
 
@@ -9,8 +10,12 @@ import  'dotenv/config';
 })();
 
 
-function main(){
-  Server.start();
+async function main(){
+  await MongoDataBase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName:envs.MONGO_DB_NAME
+  });
+//  Server.start();
 //console.log(envs);
 }
 
